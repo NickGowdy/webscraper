504b2ec92a91b2e7ab0ab5ebb6f98226c655331d
Nick Gowdy <nickgowdy87@gmail.com>
2021-11-26 15:25:39 +0000

    WIP - list of property search pages to go to.

--- lib/webscraper/modules/property_site_crawler.ex
+++ lib/webscraper/modules/property_site_crawler.ex
@@ -10,53 +10,51 @@ defmodule PropertySiteCrawler do
 
   @impl Crawly.Spider
   def parse_item(response) do
-      # Parse response body to document
-      {:ok, document} = Floki.parse_document(response.body)
+    # Parse response body to document
+    {:ok, document} = Floki.parse_document(response.body)
 
-      # Create item (for pages where items exists)
-      items =
-        document
-          |> Floki.find("main#root")
-          |> Floki.find("section")
-          |> Floki.find("a")
-          |> Floki.attribute("href")
-        |> Enum.map(fn x ->
-          %{link: x}
-        end)
+    # Create item (for pages where items exists)
+    items =
+      document
+      |> Floki.find("main#root")
+      |> Floki.find("section")
+      |> Floki.find("a")
+      |> Floki.attribute("href")
+      |> Enum.reject(fn x -> x == "/" || x == "#" end)
+      |> Enum.filter(fn x -> String.contains?(x, "/property/") end)
 
-      next_requests =
-        document
-        |> Floki.find(".next a")
-        |> Floki.attribute("href")
-        |> Enum.map(fn url ->
-          Crawly.Utils.build_absolute_url(url, response.request.url)
-          |> Crawly.Utils.request_from_url()
-        end)
-      %{items: items, requests: next_requests}
+    next_requests =
+      items
+      |> Enum.map(fn x ->
+        Crawly.Utils.build_absolute_url(x, base_url())
+        |> Crawly.Utils.request_from_url()
+      end)
+
+    %{items: items, requests: next_requests}
   end
 
-    # result =
-    #   reponse.body
-    #   |> Floki.find("main#root")
-    #   |> Floki.find("section")
-    #   |> Floki.find("a")
-    #   |> Floki.attribute("href")
+  # result =
+  #   reponse.body
+  #   |> Floki.find("main#root")
+  #   |> Floki.find("section")
+  #   |> Floki.find("a")
+  #   |> Floki.attribute("href")
 
-    # result
+  # result
 
-    # |> Map.new(fn e ->
-    #   IO.inspect(e, label: "ENTRY ")
-    #   %{link_url: e}
-    # end)
+  # |> Map.new(fn e ->
+  #   IO.inspect(e, label: "ENTRY ")
+  #   %{link_url: e}
+  # end)
 
-    # requests =
-    #   Utils.build_absolute_urls(hrefs, base_url())
-    #   |> Utils.requests_from_urls()
+  # requests =
+  #   Utils.build_absolute_urls(hrefs, base_url())
+  #   |> Utils.requests_from_urls()
 
-    # title = document |> Floki.find("h1.page-title-sm") |> Floki.text()
+  # title = document |> Floki.find("h1.page-title-sm") |> Floki.text()
 
-    # %{
-    #   :requests => requests,
-    #   :items => [%{title: title, url: response.request_url}]
-    # }
+  # %{
+  #   :requests => requests,
+  #   :items => [%{title: title, url: response.request_url}]
+  # }
 end
